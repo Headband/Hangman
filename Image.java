5e93ba34772501639030589f682fb7f771607230
JamesLR <jameslanthierrogers@ymail.com>
2017-04-20 13:17:59 -0400

    Add files via upload

---
+++ Image.java
@@ -0,0 +1,179 @@
+
+
+public class Image
+{
+   public static void hangImage ()
+   {
+       if (wrongGuess = 0)
+       {
+          
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+   
+       }
+    else if (wrongGuess = 1)
+    {
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                 _     ");
+       System.out.println("   |                               /   \\ ");
+       System.out.println("   |                              (     ) ");
+       System.out.println("   |                                \\ / ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+    }
+    else if (wrongGuess = 2)
+    {
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                 _     ");
+       System.out.println("   |                               /   \\ ");
+       System.out.println("   |                              (     ) ");
+       System.out.println("   |                                \\ / ");
+       System.out.println("   |                               __|__ ");
+       System.out.println("   |                                 |   ");
+       System.out.println("   |                                 |   ");
+       System.out.println("   |                                 |");
+       System.out.println("   |                                 | ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+    }
+   
+     else if (wrongGuess = 3)
+    {
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                 _     ");
+       System.out.println("   |                               /   \\ ");
+       System.out.println("   |                              (     ) ");
+       System.out.println("   |                                \\ / ");
+       System.out.println("   |                               __|__ ");
+       System.out.println("   |                                 |  \\ ");
+       System.out.println("   |                                 |   \\ ");
+       System.out.println("   |                                 |    \\");
+       System.out.println("   |                                 |     \\");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+    }
+   
+     else if (wrongGuess = 4)
+    {
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                 _     ");
+       System.out.println("   |                               /   \\ ");
+       System.out.println("   |                              (     ) ");
+       System.out.println("   |                                \\ / ");
+       System.out.println("   |                               __|__ ");
+       System.out.println("   |                              /  |  \\ ");
+       System.out.println("   |                             /   |   \\ ");
+       System.out.println("   |                            /    |    \\");
+       System.out.println("   |                           /     |     \\");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+    }
+   
+       else if (wrongGuess = 5)
+    {
+       System.out.println("   +----------------------------------+  ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                  | ");
+       System.out.println("   |                                 _     ");
+       System.out.println("   |                               /   \\ ");
+       System.out.println("   |                              (     ) ");
+       System.out.println("   |                                \\ / ");
+       System.out.println("   |                               __|__ ");
+       System.out.println("   |                              /  |  \\ ");
+       System.out.println("   |                             /   |   \\ ");
+       System.out.println("   |                            /    |    \\");
+       System.out.println("   |                           /     |     \\");
+       System.out.println("   |                                  \\ ");
+       System.out.println("   |                                   \\");
+       System.out.println("   |                                    \\");
+       System.out.println("   |                                     \\");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+    
+    }
+   
+    else if (wrongGuess = 6)
+    {
+       System.out.println("   +---------------------------+  ");
+       System.out.println("   |                           | ");
+       System.out.println("   |                           | ");
+       System.out.println("   |                           _     ");
+       System.out.println("   |                         /   \\ ");
+       System.out.println("   |                        (     ) ");
+       System.out.println("   |                          \\ / ");
+       System.out.println("   |                         __|__ ");
+       System.out.println("   |                        /  |   \\");
+       System.out.println("   |                       /   |    \\ ");
+       System.out.println("   |                      /    |     \\ ");
+       System.out.println("   |                     /     |      \\ ");
+       System.out.println("   |                          / \\ ");
+       System.out.println("   |                         /   \\");
+       System.out.println("   |                        /     \\");
+       System.out.println("   |                       /       \\ ");
+       System.out.println("   |  ");
+       System.out.println("   |  ");
+       System.out.println(" --^-- "); 
+       
+        
+        
+    }
+}
+}
